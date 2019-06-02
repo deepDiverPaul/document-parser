d549c4b8d48c8a88b44cea266895541209c46d67
Paul Spenke <paul@spenke.de>
2019-06-02 18:16:46 +0200

    add index.sh

---
+++ index.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Check lockfile
+if [ -f index.lock ]
+then
+    echo "----- Script is already running -----"
+    exit
+fi
+
+date +%s > index.lock
+
+# Update index
+recollindex
+
+rm index.lock
+
+echo  "----- Finish -----"
